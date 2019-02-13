765024a5f851fbee1fc54a542403a00b2cba9b67
sssena <ssena310@gmail.com>
2019-02-13 17:12:52 +0900

    using webpack instead of gulp.

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { spawn } = require('child_process')
+
+// Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
+const defaultInclude = path.resolve(__dirname, 'src')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // loader CSS
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'postcss-loader' }],
+        include: defaultInclude
+      },
+      {
+        test: /\.jsx?$/, // loader for react
+        use: [{ loader: 'babel-loader' }],
+        include: defaultInclude
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/, // loader for images
+        use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
+        include: defaultInclude
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/, // loader for custom fonts
+        use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
+        include: defaultInclude
+      }
+    ]
+  },
+  target: 'electron-renderer',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+}
