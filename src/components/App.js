cd3080ab3f7f6c71f38d610822bc1f7559fd927d
sssena <ssena310@gmail.com>
2019-02-13 17:21:07 +0900

    first component

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
     render() {
       return <div>
-          <div id='contents'></div>
+          <div id='contents'>hello world</div>
         </div> ;
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App';
+
 window.onload = () => {
-  ReactDOM.render(<div></div>, document.getElementById('app'));
+  ReactDOM.render(<App />, document.getElementById('app'));
 };
